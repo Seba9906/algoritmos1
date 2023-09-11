e9eb37f0bc09d47147a0f1f64bf5bdf842d7d265
Seba9906 <troncoso.seb.99@gmail.com>
2023-09-11 19:30:34 -0300

    .

--- guia3/ej2.hs
+++ guia3/ej2.hs
@@ -4,3 +4,8 @@ absoluto n | n >= 0 = n
 absolutoMaximo :: Int -> Int -> Int
 absolutoMaximo n m| (absoluto n) >= (absoluto m) = n
                   | otherwise = (absoluto m)
+maximo3 :: Int -> Int -> Int -> Int
+maximo3 m n o
+  | m >= n && m >= o = m
+  | n >= m && n >= o = n
+  | otherwise = o
