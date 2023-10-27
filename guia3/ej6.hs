b44cdd83d3d8dbe6471ff9787eddc0503eb6c667
Seba9906 <troncoso.seb.99@gmail.com>
2023-10-27 00:15:55 -0300

    .

--- guia3/ej6.hs
+++
@@ -1,5 +0,0 @@
-bisiesto :: Integer ->Bool
-bisiesto anio = (esMultiploDe anio 4) ||(not(esMultiploDe anio 100) && esMultiploDe anio 400)
-      where
-            esMultiploDe x y = x `mod` y == 0
-            
--- guia3/ej7.hs
+++
@@ -1,5 +0,0 @@
-distanciaManhattan:: (Float, Float, Float) ->(Float, Float, Float) -> Float
-distanciaManhattan (x,y,z) (a,b,c) | distanciaAbsoluta (x,y,z) (a,b,c) > 0 = distanciaAbsoluta (x,y,z) (a,b,c)
-                                   | otherwise = - (distanciaAbsoluta (x,y,z) (a,b,c))
-                                   where
-                                          distanciaAbsoluta (x,y,z) (a,b,c) = (x-a) + (y-b) + (z-c)
